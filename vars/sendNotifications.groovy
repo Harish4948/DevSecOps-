ef1e23d4a13dca9a3b587cd956e6de9cc9d8015e
harish4948 <pharish4948@gmail.com>
2023-05-21 12:51:30 +0800

    Fix: Slack Notification

--- vars/sendNotifications.groovy
+++ vars/sendNotifications.groovy
@@ -13,4 +13,6 @@ def call(String buildStatus = 'STARTED') {
   color = '#ec2805'
   emoji = ':hulk:'
  }
+def msg= "${buildStatus}: `${env.JOB_NAME}` #${env.BUILD_NUMBER}:\n${env.BUILD_URL}"
+  slackSend(color:color,message: msg)
 }
